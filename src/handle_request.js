d008e202f6c7917369cc94b2600ecdaffc7876f3
heartedfrank <heartedfrank@139.com>
2025-12-27 21:20:11 +0800

    print response text log

--- src/handle_request.js
+++ src/handle_request.js
@@ -87,7 +87,7 @@ export async function handleRequest(request) {
 
 
       // const { models } = JSON.parse(await response.text());
-      console.log(`Gemini response models=:`,response.text());
+      console.log(`Gemini response response.ok=:`,response.ok);
       if (response.ok) {
           const { models } = JSON.parse(await response.text());
          const body = JSON.stringify({
